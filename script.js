9c52ab93f7d5416adfa7ce7d2bd973031c566214
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-06-01 22:23:40 -0300

    requisito 9 comcluído

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const colorPaletteReference = document.querySelector('#color-palette');
 const createElementPixelBoard = document.createElement('table');
 createElementPixelBoard.setAttribute('id', 'pixel-board');
 colorPaletteReference.after(createElementPixelBoard);
-// const createElementPixelBoard = document.querySelector('#pixel-board');
 
 for (let index = 0; index < 25; index += 1) {
   const createElementoPixel = document.createElement('tr');
@@ -35,3 +34,22 @@ for (let index = 0; index < pixelsOnTheBoard.length; index += 1) {
     pixelsOnTheBoard[index].style.backgroundColor = color;
   });
 }
+
+const pixelSize = parseInt(window.getComputedStyle(document.querySelector('.pixel')).width, 10);
+const pixelBoardSize = document.getElementById('pixel-board');
+pixelBoardSize.style.width = `${pixelSize * 5 + 10}px`;
+const createElementButtonClear = document.createElement('button');
+createElementButtonClear.setAttribute('id', 'clear-board');
+colorPaletteReference.after(createElementButtonClear);
+createElementButtonClear.style.width = '70px';
+createElementButtonClear.style.height = '30px';
+createElementButtonClear.style.margin = '5px auto';
+createElementButtonClear.style.display = 'block';
+createElementButtonClear.innerText = 'Limpar';
+const buttonClearReference = document.querySelector('#clear-board');
+
+buttonClearReference.addEventListener('click', () => {
+  for (let index = 0; index < pixelsOnTheBoard.length; index += 1) {
+    pixelsOnTheBoard[index].style.backgroundColor = 'white';
+  }
+});
